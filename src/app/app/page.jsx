38700e882d6f503782835a6922fde8b5afebb4a4
nelmerayala <nelmerayala@gmail.com>
2024-09-10 16:34:39 -0400

    dashboard

--- src/app/app/page.jsx
+++ src/app/app/page.jsx
@@ -1,3 +1,204 @@
-export default async function MainApp() {
-  return <>{"Pagina principal"}</>;
+// import { Box, Input } from "@mui/material";
+// // import "./style.scss";
+
+// export default async function MainApp() {
+//   return <>Página Principal</>;
+// }
+"use client";
+
+// import * as React from "react";
+// import PropTypes from "prop-types";
+// import Tabs from "@mui/material/Tabs";
+// import Tab from "@mui/material/Tab";
+// import Box from "@mui/material/Box";
+// import { Typography } from "@mui/material";
+
+// function CustomTabPanel(props) {
+//   const { children, value, index, ...other } = props;
+
+//   return (
+//     <div
+//       role="tabpanel"
+//       hidden={value !== index}
+//       id={`simple-tabpanel-${index}`}
+//       aria-labelledby={`simple-tab-${index}`}
+//       {...other}
+//     >
+//       {value === index && <Box sx={{ p: 3 }}>{children}</Box>}
+//     </div>
+//   );
+// }
+
+// CustomTabPanel.propTypes = {
+//   children: PropTypes.node,
+//   index: PropTypes.number.isRequired,
+//   value: PropTypes.number.isRequired,
+// };
+
+// function a11yProps(index) {
+//   return {
+//     id: `simple-tab-${index}`,
+//     "aria-controls": `simple-tabpanel-${index}`,
+//   };
+// }
+
+// export default function BasicTabs() {
+//   const [value, setValue] = React.useState(0);
+
+//   const handleChange = (event, newValue) => {
+//     setValue(newValue);
+//   };
+
+//   return (
+//     <>
+//       <Typography variant="h4">
+//         Ahora puede adquirir sus estampillas con tres simples pasos
+//       </Typography>
+//       <Box sx={{ width: "100%" }}>
+//         <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
+//           <Tabs
+//             value={value}
+//             onChange={handleChange}
+//             aria-label="basic tabs example"
+//           >
+//             <Tab label="Paso 1" {...a11yProps(0)} />
+//             <Tab label="Paso 2" {...a11yProps(1)} />
+//             <Tab label="Paso 3" {...a11yProps(2)} />
+//           </Tabs>
+//         </Box>
+//         <CustomTabPanel value={value} index={0}>
+//           Paso 1
+//         </CustomTabPanel>
+//         <CustomTabPanel value={value} index={1}>
+//           Paso 2
+//         </CustomTabPanel>
+//         <CustomTabPanel value={value} index={2}>
+//           Paso 3
+//         </CustomTabPanel>
+//       </Box>
+//     </>
+//   );
+// }
+
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Stepper from "@mui/material/Stepper";
+import Step from "@mui/material/Step";
+import StepLabel from "@mui/material/StepLabel";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import { Container } from "@mui/material";
+
+const steps = ["Paso 1", "Paso 2", "Paso 3"];
+
+export default function HorizontalLinearStepper() {
+  const [activeStep, setActiveStep] = React.useState(0);
+  const [skipped, setSkipped] = React.useState(new Set());
+
+  const isStepOptional = (step) => {
+    return step === 1;
+  };
+
+  const isStepSkipped = (step) => {
+    return skipped.has(step);
+  };
+
+  const handleNext = () => {
+    let newSkipped = skipped;
+    if (isStepSkipped(activeStep)) {
+      newSkipped = new Set(newSkipped.values());
+      newSkipped.delete(activeStep);
+    }
+
+    setActiveStep((prevActiveStep) => prevActiveStep + 1);
+    setSkipped(newSkipped);
+  };
+
+  const handleBack = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep - 1);
+  };
+
+  const handleSkip = () => {
+    if (!isStepOptional(activeStep)) {
+      // You probably want to guard against something like this,
+      // it should never occur unless someone's actively trying to break something.
+      throw new Error("You can't skip a step that isn't optional.");
+    }
+
+    setActiveStep((prevActiveStep) => prevActiveStep + 1);
+    setSkipped((prevSkipped) => {
+      const newSkipped = new Set(prevSkipped.values());
+      newSkipped.add(activeStep);
+      return newSkipped;
+    });
+  };
+
+  const handleReset = () => {
+    setActiveStep(0);
+  };
+
+  return (
+    <Box sx={{ width: "100%" }}>
+      <Stepper activeStep={activeStep}>
+        {steps.map((label, index) => {
+          const stepProps = {};
+          const labelProps = {};
+          if (isStepOptional(index)) {
+            labelProps.optional = (
+              <Typography variant="caption">Opcional</Typography>
+            );
+          }
+          if (isStepSkipped(index)) {
+            stepProps.completed = false;
+          }
+          return (
+            <Step key={label} {...stepProps}>
+              <StepLabel {...labelProps}>{label}</StepLabel>
+            </Step>
+          );
+        })}
+      </Stepper>
+      {activeStep === steps.length ? (
+        <React.Fragment>
+          <Typography sx={{ mt: 2, mb: 1 }}>
+            Todos los pasos completados - Ya puedes descargar/imprimir tu
+            estampilla fiscal
+          </Typography>
+          <Box sx={{ display: "flex", flexDirection: "row", pt: 2 }}>
+            <Box sx={{ flex: "1 1 auto" }} />
+            <Button onClick={handleReset}>Reiniciar Pasos</Button>
+          </Box>
+        </React.Fragment>
+      ) : (
+        <React.Fragment>
+          <Typography sx={{ mt: 2, mb: 1 }}>Paso {activeStep + 1}</Typography>
+
+          <Container maxWidth="sm">
+            <Box sx={{ bgcolor: "#cfe8fc", height: "50vh" }} />
+          </Container>
+
+          <Box sx={{ display: "flex", flexDirection: "row", pt: 2 }}>
+            <Button
+              color="inherit"
+              disabled={activeStep === 0}
+              onClick={handleBack}
+              sx={{ mr: 1 }}
+            >
+              Atras
+            </Button>
+            <Box sx={{ flex: "1 1 auto" }} />
+            {isStepOptional(activeStep) && (
+              <Button color="inherit" onClick={handleSkip} sx={{ mr: 1 }}>
+                Saltar
+              </Button>
+            )}
+
+            <Button onClick={handleNext}>
+              {activeStep === steps.length - 1 ? "Finalizar" : "Siguiente"}
+            </Button>
+          </Box>
+        </React.Fragment>
+      )}
+    </Box>
+  );
 }
